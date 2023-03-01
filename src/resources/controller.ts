5cb3bd4af1642c1b5f97796b8d043b4327ddcb6b
oluwatolaio <oluwafemi.oluwatola@sterling.ng>
2023-03-01 14:08:31 +0100

    Created routes and controller logic for post and put

--- src/resources/controller.ts
+++ src/resources/controller.ts
@@ -1,32 +1,59 @@
-export { Request, Response } from "express";
+import { Request, Response } from "express";
+import { CreateArticleInput, UpdateArticleInput } from "./schema";
+import { string } from "zod";
+import ArticleModel from "./model";
 import {
-    CreateArticleInput,
-    UpdateArticleInput,
-  } from "./schema";
-import {
-    createArticle,
-    deleteArticle,
-    findAndUpdateArticle,
-    findArticle,
-    findAllArticles,
-  } from "./service";
+  createArticle,
+  deleteArticle,
+  findAndUpdateArticle,
+  findArticle,
+  findAllArticles,
+} from "./service";
 
-export async function createArticleHandler(req: Request, res: Response) {
+export async function createArticleHandler(
+  req: Request<{}, {}, CreateArticleInput["body"]>,
+  res: Response
+) {
+  try {
+    const body = req.body;
 
-};
+    const article = await createArticle({
+      ...body,
+      slug: "",
+    });
 
-export async function updateArticleHandler(req: Request, res: Response) {
+    return res.json({ status: 200, message: "success", data: article });
+  } catch (err) {
+    res.status(500).json({ Error });
+  }
+}
 
-};
+export async function updateArticleHandler(
+  req: Request<UpdateArticleInput["params"]>,
+  res: Response
+) {
+  try {
+    const articleId = req.params.articleId;
+    const update = req.body;
 
-export async function getArticleHandler(req: Request, res: Response) {
+    const article = await findArticle({ articleId });
 
-};
+    if (!article) {
+      return res.sendStatus(404);
+    }
 
-export async function getAllArticlesHandler(req: Request, res: Response) {
+    const updatedArticle = await findAndUpdateArticle({ articleId }, update, {
+      new: true,
+    });
 
-};
+    return res.json({ status: 200, message: "success", data: updatedArticle });
+  } catch (err) {
+    res.status(500).json({ Error });
+  }
+}
 
-export async function deleteArticleHandler(req: Request, res: Response) {
+export async function getArticleHandler(req: Request, res: Response) {}
 
-};
+export async function getAllArticlesHandler(req: Request, res: Response) {}
+
+export async function deleteArticleHandler(req: Request, res: Response) {}
--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,28 @@
 import { Express, Request, Response } from "express";
+import validateResource from "./resources/middleware";
+import {
+  createArticleHandler,
+  updateArticleHandler
+} from "./resources/controller";
+import {
+  createArticleSchema,
+  updateArticleSchema
+} from "./resources/schema";;
 
 function routes(app: Express) {
     app.get("/healthcheck", (req:Request, res:Response) => res.sendStatus(200));
+
+    app.post(
+        "/api/articles",
+        validateResource(createArticleSchema),
+        createArticleHandler
+      );
+    
+      app.put(
+        "/api/articles/:articleId",
+        validateResource(updateArticleSchema),
+        updateArticleHandler
+      );
 }
 
 export default routes;
